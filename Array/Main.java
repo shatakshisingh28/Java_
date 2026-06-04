b42177e5d3ddb8d5ec1cbc9d35a715807adf30d3
shatakshisingh28 <shatakshis@gmail.com>
2026-06-04 16:04:26 +0530

    2nd question

---
+++ Array/Main.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+class Solution {
+
+    public static int secondSmallest(int[] arr, int n) {
+        if (n < 2)
+            return -1;
+
+        int small = Integer.MAX_VALUE;
+        int second_small = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] < small) {
+                second_small = small;
+                small = arr[i];
+            } else if (arr[i] < second_small && arr[i] != small) {
+                second_small = arr[i];
+            }
+        }
+        return second_small;
+    }
+
+    public static int secondLargest(int[] arr, int n) {
+
+        if (n < 2)
+            return -1;
+
+        int large = Integer.MIN_VALUE, second_large = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > large) {
+                second_large = large;
+                large = arr[i];
+            } else if (arr[i] > second_large && arr[i] != large) {
+                second_large = arr[i];
+            }
+        }
+        return second_large;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sS = Solution.secondSmallest(arr, n);
+        int sL = Solution.secondLargest(arr, n);
+
+        System.out.println("Second smallest is " + sS);
+        System.out.println("Second largest is " + sL);
+    sc.close();
+    }
+}
